40e7db38fe41e829782adf431d5139eb28136ca6
liuyan9 <liuyan90806@proton.me>
2023-07-23 09:18:12 +0000

    New translations en.json (German) [ci skip] Signed-off-by: Björn Kimminich <bjoern.kimminich@gmx.de>

--- public/js/webflow.js
+++ public/js/webflow.js
@@ -6464,3 +6464,494 @@
         }
         if (isSymbol(value)) {
           return NAN;
+        }
+        if (isObject(value)) {
+          var other = typeof value.valueOf == "function" ? value.valueOf() : value;
+          value = isObject(other) ? other + "" : other;
+        }
+        if (typeof value != "string") {
+          return value === 0 ? value : +value;
+        }
+        value = baseTrim(value);
+        var isBinary = reIsBinary.test(value);
+        return isBinary || reIsOctal.test(value) ? freeParseInt(value.slice(2), isBinary ? 2 : 8) : reIsBadHex.test(value) ? NAN : +value;
+      }
+      module.exports = toNumber;
+    }
+  });
+
+  // node_modules/lodash/toFinite.js
+  var require_toFinite = __commonJS({
+    "node_modules/lodash/toFinite.js"(exports, module) {
+      var toNumber = require_toNumber();
+      var INFINITY = 1 / 0;
+      var MAX_INTEGER = 17976931348623157e292;
+      function toFinite(value) {
+        if (!value) {
+          return value === 0 ? value : 0;
+        }
+        value = toNumber(value);
+        if (value === INFINITY || value === -INFINITY) {
+          var sign = value < 0 ? -1 : 1;
+          return sign * MAX_INTEGER;
+        }
+        return value === value ? value : 0;
+      }
+      module.exports = toFinite;
+    }
+  });
+
+  // node_modules/lodash/toInteger.js
+  var require_toInteger = __commonJS({
+    "node_modules/lodash/toInteger.js"(exports, module) {
+      var toFinite = require_toFinite();
+      function toInteger(value) {
+        var result = toFinite(value), remainder = result % 1;
+        return result === result ? remainder ? result - remainder : result : 0;
+      }
+      module.exports = toInteger;
+    }
+  });
+
+  // node_modules/lodash/findIndex.js
+  var require_findIndex = __commonJS({
+    "node_modules/lodash/findIndex.js"(exports, module) {
+      var baseFindIndex = require_baseFindIndex();
+      var baseIteratee = require_baseIteratee();
+      var toInteger = require_toInteger();
+      var nativeMax = Math.max;
+      function findIndex(array, predicate, fromIndex) {
+        var length = array == null ? 0 : array.length;
+        if (!length) {
+          return -1;
+        }
+        var index = fromIndex == null ? 0 : toInteger(fromIndex);
+        if (index < 0) {
+          index = nativeMax(length + index, 0);
+        }
+        return baseFindIndex(array, baseIteratee(predicate, 3), index);
+      }
+      module.exports = findIndex;
+    }
+  });
+
+  // node_modules/lodash/find.js
+  var require_find = __commonJS({
+    "node_modules/lodash/find.js"(exports, module) {
+      var createFind = require_createFind();
+      var findIndex = require_findIndex();
+      var find = createFind(findIndex);
+      module.exports = find;
+    }
+  });
+
+  // packages/systems/ix2/shared/logic/IX2BrowserSupport.js
+  var require_IX2BrowserSupport = __commonJS({
+    "packages/systems/ix2/shared/logic/IX2BrowserSupport.js"(exports) {
+      "use strict";
+      var _interopRequireDefault = require_interopRequireDefault().default;
+      Object.defineProperty(exports, "__esModule", {
+        value: true
+      });
+      exports.withBrowser = exports.TRANSFORM_STYLE_PREFIXED = exports.TRANSFORM_PREFIXED = exports.IS_BROWSER_ENV = exports.FLEX_PREFIXED = exports.ELEMENT_MATCHES = void 0;
+      var _find = _interopRequireDefault(require_find());
+      var IS_BROWSER_ENV = typeof window !== "undefined";
+      exports.IS_BROWSER_ENV = IS_BROWSER_ENV;
+      var withBrowser = (fn, fallback) => {
+        if (IS_BROWSER_ENV) {
+          return fn();
+        }
+        return fallback;
+      };
+      exports.withBrowser = withBrowser;
+      var ELEMENT_MATCHES = withBrowser(() => {
+        return (0, _find.default)(["matches", "matchesSelector", "mozMatchesSelector", "msMatchesSelector", "oMatchesSelector", "webkitMatchesSelector"], (key) => key in Element.prototype);
+      });
+      exports.ELEMENT_MATCHES = ELEMENT_MATCHES;
+      var FLEX_PREFIXED = withBrowser(() => {
+        const el = document.createElement("i");
+        const values = ["flex", "-webkit-flex", "-ms-flexbox", "-moz-box", "-webkit-box"];
+        const none = "";
+        try {
+          const {
+            length
+          } = values;
+          for (let i = 0; i < length; i++) {
+            const value = values[i];
+            el.style.display = value;
+            if (el.style.display === value) {
+              return value;
+            }
+          }
+          return none;
+        } catch (err) {
+          return none;
+        }
+      }, "flex");
+      exports.FLEX_PREFIXED = FLEX_PREFIXED;
+      var TRANSFORM_PREFIXED = withBrowser(() => {
+        const el = document.createElement("i");
+        if (el.style.transform == null) {
+          const prefixes = ["Webkit", "Moz", "ms"];
+          const suffix = "Transform";
+          const {
+            length
+          } = prefixes;
+          for (let i = 0; i < length; i++) {
+            const prop = prefixes[i] + suffix;
+            if (el.style[prop] !== void 0) {
+              return prop;
+            }
+          }
+        }
+        return "transform";
+      }, "transform");
+      exports.TRANSFORM_PREFIXED = TRANSFORM_PREFIXED;
+      var TRANSFORM_PREFIX = TRANSFORM_PREFIXED.split("transform")[0];
+      var TRANSFORM_STYLE_PREFIXED = TRANSFORM_PREFIX ? TRANSFORM_PREFIX + "TransformStyle" : "transformStyle";
+      exports.TRANSFORM_STYLE_PREFIXED = TRANSFORM_STYLE_PREFIXED;
+    }
+  });
+
+  // node_modules/bezier-easing/src/index.js
+  var require_src = __commonJS({
+    "node_modules/bezier-easing/src/index.js"(exports, module) {
+      var NEWTON_ITERATIONS = 4;
+      var NEWTON_MIN_SLOPE = 1e-3;
+      var SUBDIVISION_PRECISION = 1e-7;
+      var SUBDIVISION_MAX_ITERATIONS = 10;
+      var kSplineTableSize = 11;
+      var kSampleStepSize = 1 / (kSplineTableSize - 1);
+      var float32ArraySupported = typeof Float32Array === "function";
+      function A(aA1, aA2) {
+        return 1 - 3 * aA2 + 3 * aA1;
+      }
+      function B(aA1, aA2) {
+        return 3 * aA2 - 6 * aA1;
+      }
+      function C(aA1) {
+        return 3 * aA1;
+      }
+      function calcBezier(aT, aA1, aA2) {
+        return ((A(aA1, aA2) * aT + B(aA1, aA2)) * aT + C(aA1)) * aT;
+      }
+      function getSlope(aT, aA1, aA2) {
+        return 3 * A(aA1, aA2) * aT * aT + 2 * B(aA1, aA2) * aT + C(aA1);
+      }
+      function binarySubdivide(aX, aA, aB, mX1, mX2) {
+        var currentX, currentT, i = 0;
+        do {
+          currentT = aA + (aB - aA) / 2;
+          currentX = calcBezier(currentT, mX1, mX2) - aX;
+          if (currentX > 0) {
+            aB = currentT;
+          } else {
+            aA = currentT;
+          }
+        } while (Math.abs(currentX) > SUBDIVISION_PRECISION && ++i < SUBDIVISION_MAX_ITERATIONS);
+        return currentT;
+      }
+      function newtonRaphsonIterate(aX, aGuessT, mX1, mX2) {
+        for (var i = 0; i < NEWTON_ITERATIONS; ++i) {
+          var currentSlope = getSlope(aGuessT, mX1, mX2);
+          if (currentSlope === 0) {
+            return aGuessT;
+          }
+          var currentX = calcBezier(aGuessT, mX1, mX2) - aX;
+          aGuessT -= currentX / currentSlope;
+        }
+        return aGuessT;
+      }
+      module.exports = function bezier(mX1, mY1, mX2, mY2) {
+        if (!(0 <= mX1 && mX1 <= 1 && 0 <= mX2 && mX2 <= 1)) {
+          throw new Error("bezier x values must be in [0, 1] range");
+        }
+        var sampleValues = float32ArraySupported ? new Float32Array(kSplineTableSize) : new Array(kSplineTableSize);
+        if (mX1 !== mY1 || mX2 !== mY2) {
+          for (var i = 0; i < kSplineTableSize; ++i) {
+            sampleValues[i] = calcBezier(i * kSampleStepSize, mX1, mX2);
+          }
+        }
+        function getTForX(aX) {
+          var intervalStart = 0;
+          var currentSample = 1;
+          var lastSample = kSplineTableSize - 1;
+          for (; currentSample !== lastSample && sampleValues[currentSample] <= aX; ++currentSample) {
+            intervalStart += kSampleStepSize;
+          }
+          --currentSample;
+          var dist = (aX - sampleValues[currentSample]) / (sampleValues[currentSample + 1] - sampleValues[currentSample]);
+          var guessForT = intervalStart + dist * kSampleStepSize;
+          var initialSlope = getSlope(guessForT, mX1, mX2);
+          if (initialSlope >= NEWTON_MIN_SLOPE) {
+            return newtonRaphsonIterate(aX, guessForT, mX1, mX2);
+          } else if (initialSlope === 0) {
+            return guessForT;
+          } else {
+            return binarySubdivide(aX, intervalStart, intervalStart + kSampleStepSize, mX1, mX2);
+          }
+        }
+        return function BezierEasing(x) {
+          if (mX1 === mY1 && mX2 === mY2) {
+            return x;
+          }
+          if (x === 0) {
+            return 0;
+          }
+          if (x === 1) {
+            return 1;
+          }
+          return calcBezier(getTForX(x), mY1, mY2);
+        };
+      };
+    }
+  });
+
+  // packages/systems/ix2/shared/logic/IX2Easings.js
+  var require_IX2Easings = __commonJS({
+    "packages/systems/ix2/shared/logic/IX2Easings.js"(exports) {
+      "use strict";
+      var _interopRequireDefault = require_interopRequireDefault().default;
+      Object.defineProperty(exports, "__esModule", {
+        value: true
+      });
+      exports.bounce = bounce;
+      exports.bouncePast = bouncePast;
+      exports.easeOut = exports.easeInOut = exports.easeIn = exports.ease = void 0;
+      exports.inBack = inBack;
+      exports.inCirc = inCirc;
+      exports.inCubic = inCubic;
+      exports.inElastic = inElastic;
+      exports.inExpo = inExpo;
+      exports.inOutBack = inOutBack;
+      exports.inOutCirc = inOutCirc;
+      exports.inOutCubic = inOutCubic;
+      exports.inOutElastic = inOutElastic;
+      exports.inOutExpo = inOutExpo;
+      exports.inOutQuad = inOutQuad;
+      exports.inOutQuart = inOutQuart;
+      exports.inOutQuint = inOutQuint;
+      exports.inOutSine = inOutSine;
+      exports.inQuad = inQuad;
+      exports.inQuart = inQuart;
+      exports.inQuint = inQuint;
+      exports.inSine = inSine;
+      exports.outBack = outBack;
+      exports.outBounce = outBounce;
+      exports.outCirc = outCirc;
+      exports.outCubic = outCubic;
+      exports.outElastic = outElastic;
+      exports.outExpo = outExpo;
+      exports.outQuad = outQuad;
+      exports.outQuart = outQuart;
+      exports.outQuint = outQuint;
+      exports.outSine = outSine;
+      exports.swingFrom = swingFrom;
+      exports.swingFromTo = swingFromTo;
+      exports.swingTo = swingTo;
+      var _bezierEasing = _interopRequireDefault(require_src());
+      var magicSwing = 1.70158;
+      var ease = (0, _bezierEasing.default)(0.25, 0.1, 0.25, 1);
+      exports.ease = ease;
+      var easeIn = (0, _bezierEasing.default)(0.42, 0, 1, 1);
+      exports.easeIn = easeIn;
+      var easeOut = (0, _bezierEasing.default)(0, 0, 0.58, 1);
+      exports.easeOut = easeOut;
+      var easeInOut = (0, _bezierEasing.default)(0.42, 0, 0.58, 1);
+      exports.easeInOut = easeInOut;
+      function inQuad(pos) {
+        return Math.pow(pos, 2);
+      }
+      function outQuad(pos) {
+        return -(Math.pow(pos - 1, 2) - 1);
+      }
+      function inOutQuad(pos) {
+        if ((pos /= 0.5) < 1) {
+          return 0.5 * Math.pow(pos, 2);
+        }
+        return -0.5 * ((pos -= 2) * pos - 2);
+      }
+      function inCubic(pos) {
+        return Math.pow(pos, 3);
+      }
+      function outCubic(pos) {
+        return Math.pow(pos - 1, 3) + 1;
+      }
+      function inOutCubic(pos) {
+        if ((pos /= 0.5) < 1) {
+          return 0.5 * Math.pow(pos, 3);
+        }
+        return 0.5 * (Math.pow(pos - 2, 3) + 2);
+      }
+      function inQuart(pos) {
+        return Math.pow(pos, 4);
+      }
+      function outQuart(pos) {
+        return -(Math.pow(pos - 1, 4) - 1);
+      }
+      function inOutQuart(pos) {
+        if ((pos /= 0.5) < 1) {
+          return 0.5 * Math.pow(pos, 4);
+        }
+        return -0.5 * ((pos -= 2) * Math.pow(pos, 3) - 2);
+      }
+      function inQuint(pos) {
+        return Math.pow(pos, 5);
+      }
+      function outQuint(pos) {
+        return Math.pow(pos - 1, 5) + 1;
+      }
+      function inOutQuint(pos) {
+        if ((pos /= 0.5) < 1) {
+          return 0.5 * Math.pow(pos, 5);
+        }
+        return 0.5 * (Math.pow(pos - 2, 5) + 2);
+      }
+      function inSine(pos) {
+        return -Math.cos(pos * (Math.PI / 2)) + 1;
+      }
+      function outSine(pos) {
+        return Math.sin(pos * (Math.PI / 2));
+      }
+      function inOutSine(pos) {
+        return -0.5 * (Math.cos(Math.PI * pos) - 1);
+      }
+      function inExpo(pos) {
+        return pos === 0 ? 0 : Math.pow(2, 10 * (pos - 1));
+      }
+      function outExpo(pos) {
+        return pos === 1 ? 1 : -Math.pow(2, -10 * pos) + 1;
+      }
+      function inOutExpo(pos) {
+        if (pos === 0) {
+          return 0;
+        }
+        if (pos === 1) {
+          return 1;
+        }
+        if ((pos /= 0.5) < 1) {
+          return 0.5 * Math.pow(2, 10 * (pos - 1));
+        }
+        return 0.5 * (-Math.pow(2, -10 * --pos) + 2);
+      }
+      function inCirc(pos) {
+        return -(Math.sqrt(1 - pos * pos) - 1);
+      }
+      function outCirc(pos) {
+        return Math.sqrt(1 - Math.pow(pos - 1, 2));
+      }
+      function inOutCirc(pos) {
+        if ((pos /= 0.5) < 1) {
+          return -0.5 * (Math.sqrt(1 - pos * pos) - 1);
+        }
+        return 0.5 * (Math.sqrt(1 - (pos -= 2) * pos) + 1);
+      }
+      function outBounce(pos) {
+        if (pos < 1 / 2.75) {
+          return 7.5625 * pos * pos;
+        } else if (pos < 2 / 2.75) {
+          return 7.5625 * (pos -= 1.5 / 2.75) * pos + 0.75;
+        } else if (pos < 2.5 / 2.75) {
+          return 7.5625 * (pos -= 2.25 / 2.75) * pos + 0.9375;
+        } else {
+          return 7.5625 * (pos -= 2.625 / 2.75) * pos + 0.984375;
+        }
+      }
+      function inBack(pos) {
+        const s = magicSwing;
+        return pos * pos * ((s + 1) * pos - s);
+      }
+      function outBack(pos) {
+        const s = magicSwing;
+        return (pos -= 1) * pos * ((s + 1) * pos + s) + 1;
+      }
+      function inOutBack(pos) {
+        let s = magicSwing;
+        if ((pos /= 0.5) < 1) {
+          return 0.5 * (pos * pos * (((s *= 1.525) + 1) * pos - s));
+        }
+        return 0.5 * ((pos -= 2) * pos * (((s *= 1.525) + 1) * pos + s) + 2);
+      }
+      function inElastic(pos) {
+        let s = magicSwing;
+        let p = 0;
+        let a = 1;
+        if (pos === 0) {
+          return 0;
+        }
+        if (pos === 1) {
+          return 1;
+        }
+        if (!p) {
+          p = 0.3;
+        }
+        if (a < 1) {
+          a = 1;
+          s = p / 4;
+        } else {
+          s = p / (2 * Math.PI) * Math.asin(1 / a);
+        }
+        return -(a * Math.pow(2, 10 * (pos -= 1)) * Math.sin((pos - s) * (2 * Math.PI) / p));
+      }
+      function outElastic(pos) {
+        let s = magicSwing;
+        let p = 0;
+        let a = 1;
+        if (pos === 0) {
+          return 0;
+        }
+        if (pos === 1) {
+          return 1;
+        }
+        if (!p) {
+          p = 0.3;
+        }
+        if (a < 1) {
+          a = 1;
+          s = p / 4;
+        } else {
+          s = p / (2 * Math.PI) * Math.asin(1 / a);
+        }
+        return a * Math.pow(2, -10 * pos) * Math.sin((pos - s) * (2 * Math.PI) / p) + 1;
+      }
+      function inOutElastic(pos) {
+        let s = magicSwing;
+        let p = 0;
+        let a = 1;
+        if (pos === 0) {
+          return 0;
+        }
+        if ((pos /= 1 / 2) === 2) {
+          return 1;
+        }
+        if (!p) {
+          p = 0.3 * 1.5;
+        }
+        if (a < 1) {
+          a = 1;
+          s = p / 4;
+        } else {
+          s = p / (2 * Math.PI) * Math.asin(1 / a);
+        }
+        if (pos < 1) {
+          return -0.5 * (a * Math.pow(2, 10 * (pos -= 1)) * Math.sin((pos - s) * (2 * Math.PI) / p));
+        }
+        return a * Math.pow(2, -10 * (pos -= 1)) * Math.sin((pos - s) * (2 * Math.PI) / p) * 0.5 + 1;
+      }
+      function swingFromTo(pos) {
+        let s = magicSwing;
+        return (pos /= 0.5) < 1 ? 0.5 * (pos * pos * (((s *= 1.525) + 1) * pos - s)) : 0.5 * ((pos -= 2) * pos * (((s *= 1.525) + 1) * pos + s) + 2);
+      }
+      function swingFrom(pos) {
+        const s = magicSwing;
+        return pos * pos * ((s + 1) * pos - s);
+      }
+      function swingTo(pos) {
+        const s = magicSwing;
+        return (pos -= 1) * pos * ((s + 1) * pos + s) + 1;
+      }
+      function bounce(pos) {
+        if (pos < 1 / 2.75) {
+          return 7.5625 * pos * pos;
+        } else if (pos < 2 / 2.75) {
+          return 7.5625 * (pos -= 1.5 / 2.75) * pos + 0.75;
