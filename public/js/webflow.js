7186987dba5fc6ed8345fd495ca5cc3056879855
liuyan9 <liuyan90806@proton.me>
2023-09-14 11:58:21 +0000

    Bump version to 4.0.2

--- public/js/webflow.js
+++ public/js/webflow.js
@@ -19078,3 +19078,419 @@
       }
       function flushSubscription(subscription) {
         var queue = subscription._queue;
+        if (!queue) {
+          return;
+        }
+        subscription._queue = void 0;
+        subscription._state = "ready";
+        for (var i = 0; i < queue.length; ++i) {
+          notifySubscription(subscription, queue[i].type, queue[i].value);
+          if (subscription._state === "closed")
+            break;
+        }
+      }
+      function notifySubscription(subscription, type, value) {
+        subscription._state = "running";
+        var observer = subscription._observer;
+        try {
+          var m = getMethod(observer, type);
+          switch (type) {
+            case "next":
+              if (m)
+                m.call(observer, value);
+              break;
+            case "error":
+              closeSubscription(subscription);
+              if (m)
+                m.call(observer, value);
+              else
+                throw value;
+              break;
+            case "complete":
+              closeSubscription(subscription);
+              if (m)
+                m.call(observer);
+              break;
+          }
+        } catch (e) {
+          hostReportError(e);
+        }
+        if (subscription._state === "closed")
+          cleanupSubscription(subscription);
+        else if (subscription._state === "running")
+          subscription._state = "ready";
+      }
+      function onNotify(subscription, type, value) {
+        if (subscription._state === "closed")
+          return;
+        if (subscription._state === "buffering") {
+          subscription._queue.push({ type, value });
+          return;
+        }
+        if (subscription._state !== "ready") {
+          subscription._state = "buffering";
+          subscription._queue = [{ type, value }];
+          enqueue(function() {
+            return flushSubscription(subscription);
+          });
+          return;
+        }
+        notifySubscription(subscription, type, value);
+      }
+      var Subscription = function() {
+        function Subscription2(observer, subscriber) {
+          _classCallCheck(this, Subscription2);
+          this._cleanup = void 0;
+          this._observer = observer;
+          this._queue = void 0;
+          this._state = "initializing";
+          var subscriptionObserver = new SubscriptionObserver(this);
+          try {
+            this._cleanup = subscriber.call(void 0, subscriptionObserver);
+          } catch (e) {
+            subscriptionObserver.error(e);
+          }
+          if (this._state === "initializing")
+            this._state = "ready";
+        }
+        _createClass(Subscription2, [{
+          key: "unsubscribe",
+          value: function unsubscribe() {
+            if (this._state !== "closed") {
+              closeSubscription(this);
+              cleanupSubscription(this);
+            }
+          }
+        }, {
+          key: "closed",
+          get: function() {
+            return this._state === "closed";
+          }
+        }]);
+        return Subscription2;
+      }();
+      var SubscriptionObserver = function() {
+        function SubscriptionObserver2(subscription) {
+          _classCallCheck(this, SubscriptionObserver2);
+          this._subscription = subscription;
+        }
+        _createClass(SubscriptionObserver2, [{
+          key: "next",
+          value: function next(value) {
+            onNotify(this._subscription, "next", value);
+          }
+        }, {
+          key: "error",
+          value: function error(value) {
+            onNotify(this._subscription, "error", value);
+          }
+        }, {
+          key: "complete",
+          value: function complete() {
+            onNotify(this._subscription, "complete");
+          }
+        }, {
+          key: "closed",
+          get: function() {
+            return this._subscription._state === "closed";
+          }
+        }]);
+        return SubscriptionObserver2;
+      }();
+      var Observable = exports.Observable = function() {
+        function Observable2(subscriber) {
+          _classCallCheck(this, Observable2);
+          if (!(this instanceof Observable2))
+            throw new TypeError("Observable cannot be called as a function");
+          if (typeof subscriber !== "function")
+            throw new TypeError("Observable initializer must be a function");
+          this._subscriber = subscriber;
+        }
+        _createClass(Observable2, [{
+          key: "subscribe",
+          value: function subscribe(observer) {
+            if (typeof observer !== "object" || observer === null) {
+              observer = {
+                next: observer,
+                error: arguments[1],
+                complete: arguments[2]
+              };
+            }
+            return new Subscription(observer, this._subscriber);
+          }
+        }, {
+          key: "forEach",
+          value: function forEach(fn) {
+            var _this = this;
+            return new Promise(function(resolve2, reject2) {
+              if (typeof fn !== "function") {
+                reject2(new TypeError(fn + " is not a function"));
+                return;
+              }
+              function done() {
+                subscription.unsubscribe();
+                resolve2();
+              }
+              var subscription = _this.subscribe({
+                next: function(value) {
+                  try {
+                    fn(value, done);
+                  } catch (e) {
+                    reject2(e);
+                    subscription.unsubscribe();
+                  }
+                },
+                error: reject2,
+                complete: resolve2
+              });
+            });
+          }
+        }, {
+          key: "map",
+          value: function map(fn) {
+            var _this2 = this;
+            if (typeof fn !== "function")
+              throw new TypeError(fn + " is not a function");
+            var C = getSpecies(this);
+            return new C(function(observer) {
+              return _this2.subscribe({
+                next: function(value) {
+                  try {
+                    value = fn(value);
+                  } catch (e) {
+                    return observer.error(e);
+                  }
+                  observer.next(value);
+                },
+                error: function(e) {
+                  observer.error(e);
+                },
+                complete: function() {
+                  observer.complete();
+                }
+              });
+            });
+          }
+        }, {
+          key: "filter",
+          value: function filter(fn) {
+            var _this3 = this;
+            if (typeof fn !== "function")
+              throw new TypeError(fn + " is not a function");
+            var C = getSpecies(this);
+            return new C(function(observer) {
+              return _this3.subscribe({
+                next: function(value) {
+                  try {
+                    if (!fn(value))
+                      return;
+                  } catch (e) {
+                    return observer.error(e);
+                  }
+                  observer.next(value);
+                },
+                error: function(e) {
+                  observer.error(e);
+                },
+                complete: function() {
+                  observer.complete();
+                }
+              });
+            });
+          }
+        }, {
+          key: "reduce",
+          value: function reduce(fn) {
+            var _this4 = this;
+            if (typeof fn !== "function")
+              throw new TypeError(fn + " is not a function");
+            var C = getSpecies(this);
+            var hasSeed = arguments.length > 1;
+            var hasValue = false;
+            var seed = arguments[1];
+            var acc = seed;
+            return new C(function(observer) {
+              return _this4.subscribe({
+                next: function(value) {
+                  var first = !hasValue;
+                  hasValue = true;
+                  if (!first || hasSeed) {
+                    try {
+                      acc = fn(acc, value);
+                    } catch (e) {
+                      return observer.error(e);
+                    }
+                  } else {
+                    acc = value;
+                  }
+                },
+                error: function(e) {
+                  observer.error(e);
+                },
+                complete: function() {
+                  if (!hasValue && !hasSeed)
+                    return observer.error(new TypeError("Cannot reduce an empty sequence"));
+                  observer.next(acc);
+                  observer.complete();
+                }
+              });
+            });
+          }
+        }, {
+          key: "concat",
+          value: function concat() {
+            var _this5 = this;
+            for (var _len = arguments.length, sources = Array(_len), _key = 0; _key < _len; _key++) {
+              sources[_key] = arguments[_key];
+            }
+            var C = getSpecies(this);
+            return new C(function(observer) {
+              var subscription = void 0;
+              function startNext(next) {
+                subscription = next.subscribe({
+                  next: function(v) {
+                    observer.next(v);
+                  },
+                  error: function(e) {
+                    observer.error(e);
+                  },
+                  complete: function() {
+                    if (sources.length === 0) {
+                      subscription = void 0;
+                      observer.complete();
+                    } else {
+                      startNext(C.from(sources.shift()));
+                    }
+                  }
+                });
+              }
+              startNext(_this5);
+              return function() {
+                if (subscription) {
+                  subscription = void 0;
+                  subscription.unsubscribe();
+                }
+              };
+            });
+          }
+        }, {
+          key: "flatMap",
+          value: function flatMap(fn) {
+            var _this6 = this;
+            if (typeof fn !== "function")
+              throw new TypeError(fn + " is not a function");
+            var C = getSpecies(this);
+            return new C(function(observer) {
+              var subscriptions = [];
+              var outer = _this6.subscribe({
+                next: function(value) {
+                  if (fn) {
+                    try {
+                      value = fn(value);
+                    } catch (e) {
+                      return observer.error(e);
+                    }
+                  }
+                  var inner = C.from(value).subscribe({
+                    next: function(value2) {
+                      observer.next(value2);
+                    },
+                    error: function(e) {
+                      observer.error(e);
+                    },
+                    complete: function() {
+                      var i = subscriptions.indexOf(inner);
+                      if (i >= 0)
+                        subscriptions.splice(i, 1);
+                      completeIfDone();
+                    }
+                  });
+                  subscriptions.push(inner);
+                },
+                error: function(e) {
+                  observer.error(e);
+                },
+                complete: function() {
+                  completeIfDone();
+                }
+              });
+              function completeIfDone() {
+                if (outer.closed && subscriptions.length === 0)
+                  observer.complete();
+              }
+              return function() {
+                subscriptions.forEach(function(s) {
+                  return s.unsubscribe();
+                });
+                outer.unsubscribe();
+              };
+            });
+          }
+        }, {
+          key: getSymbol("observable"),
+          value: function() {
+            return this;
+          }
+        }], [{
+          key: "from",
+          value: function from(x) {
+            var C = typeof this === "function" ? this : Observable2;
+            if (x == null)
+              throw new TypeError(x + " is not an object");
+            var method = getMethod(x, getSymbol("observable"));
+            if (method) {
+              var observable = method.call(x);
+              if (Object(observable) !== observable)
+                throw new TypeError(observable + " is not an object");
+              if (isObservable(observable) && observable.constructor === C)
+                return observable;
+              return new C(function(observer) {
+                return observable.subscribe(observer);
+              });
+            }
+            if (hasSymbol("iterator")) {
+              method = getMethod(x, getSymbol("iterator"));
+              if (method) {
+                return new C(function(observer) {
+                  enqueue(function() {
+                    if (observer.closed)
+                      return;
+                    var _iteratorNormalCompletion = true;
+                    var _didIteratorError = false;
+                    var _iteratorError = void 0;
+                    try {
+                      for (var _iterator = method.call(x)[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+                        var item = _step.value;
+                        observer.next(item);
+                        if (observer.closed)
+                          return;
+                      }
+                    } catch (err) {
+                      _didIteratorError = true;
+                      _iteratorError = err;
+                    } finally {
+                      try {
+                        if (!_iteratorNormalCompletion && _iterator.return) {
+                          _iterator.return();
+                        }
+                      } finally {
+                        if (_didIteratorError) {
+                          throw _iteratorError;
+                        }
+                      }
+                    }
+                    observer.complete();
+                  });
+                });
+              }
+            }
+            if (Array.isArray(x)) {
+              return new C(function(observer) {
+                enqueue(function() {
+                  if (observer.closed)
+                    return;
+                  for (var i = 0; i < x.length; ++i) {
+                    observer.next(x[i]);
+                    if (observer.closed)
+                      return;
+                  }
