26e224c2ab5b35750669f3665e459606dccef561
liuyan9 <liuyan90806@proton.me>
2024-02-18 15:40:54 +0000

    - version update

--- public/js/webflow.js
+++ public/js/webflow.js
@@ -46946,3 +46946,450 @@
   var require_Transformers2 = __commonJS({
     "packages/systems/dynamo/utils/Transformers/index.js"(exports) {
       "use strict";
+      Object.defineProperty(exports, "__esModule", {
+        value: true
+      });
+      var _Transformers = require_Transformers();
+      Object.keys(_Transformers).forEach(function(key) {
+        if (key === "default" || key === "__esModule")
+          return;
+        if (key in exports && exports[key] === _Transformers[key])
+          return;
+        Object.defineProperty(exports, key, {
+          enumerable: true,
+          get: function() {
+            return _Transformers[key];
+          }
+        });
+      });
+    }
+  });
+
+  // node_modules/lodash/_baseAssign.js
+  var require_baseAssign = __commonJS({
+    "node_modules/lodash/_baseAssign.js"(exports, module) {
+      var copyObject = require_copyObject();
+      var keys = require_keys();
+      function baseAssign(object, source) {
+        return object && copyObject(source, keys(source), object);
+      }
+      module.exports = baseAssign;
+    }
+  });
+
+  // node_modules/lodash/_baseAssignIn.js
+  var require_baseAssignIn = __commonJS({
+    "node_modules/lodash/_baseAssignIn.js"(exports, module) {
+      var copyObject = require_copyObject();
+      var keysIn = require_keysIn();
+      function baseAssignIn(object, source) {
+        return object && copyObject(source, keysIn(source), object);
+      }
+      module.exports = baseAssignIn;
+    }
+  });
+
+  // node_modules/lodash/_copySymbols.js
+  var require_copySymbols = __commonJS({
+    "node_modules/lodash/_copySymbols.js"(exports, module) {
+      var copyObject = require_copyObject();
+      var getSymbols = require_getSymbols();
+      function copySymbols(source, object) {
+        return copyObject(source, getSymbols(source), object);
+      }
+      module.exports = copySymbols;
+    }
+  });
+
+  // node_modules/lodash/_copySymbolsIn.js
+  var require_copySymbolsIn = __commonJS({
+    "node_modules/lodash/_copySymbolsIn.js"(exports, module) {
+      var copyObject = require_copyObject();
+      var getSymbolsIn = require_getSymbolsIn();
+      function copySymbolsIn(source, object) {
+        return copyObject(source, getSymbolsIn(source), object);
+      }
+      module.exports = copySymbolsIn;
+    }
+  });
+
+  // node_modules/lodash/_initCloneArray.js
+  var require_initCloneArray = __commonJS({
+    "node_modules/lodash/_initCloneArray.js"(exports, module) {
+      var objectProto = Object.prototype;
+      var hasOwnProperty = objectProto.hasOwnProperty;
+      function initCloneArray(array) {
+        var length = array.length, result = new array.constructor(length);
+        if (length && typeof array[0] == "string" && hasOwnProperty.call(array, "index")) {
+          result.index = array.index;
+          result.input = array.input;
+        }
+        return result;
+      }
+      module.exports = initCloneArray;
+    }
+  });
+
+  // node_modules/lodash/_cloneDataView.js
+  var require_cloneDataView = __commonJS({
+    "node_modules/lodash/_cloneDataView.js"(exports, module) {
+      var cloneArrayBuffer = require_cloneArrayBuffer();
+      function cloneDataView(dataView, isDeep) {
+        var buffer = isDeep ? cloneArrayBuffer(dataView.buffer) : dataView.buffer;
+        return new dataView.constructor(buffer, dataView.byteOffset, dataView.byteLength);
+      }
+      module.exports = cloneDataView;
+    }
+  });
+
+  // node_modules/lodash/_cloneRegExp.js
+  var require_cloneRegExp = __commonJS({
+    "node_modules/lodash/_cloneRegExp.js"(exports, module) {
+      var reFlags = /\w*$/;
+      function cloneRegExp(regexp) {
+        var result = new regexp.constructor(regexp.source, reFlags.exec(regexp));
+        result.lastIndex = regexp.lastIndex;
+        return result;
+      }
+      module.exports = cloneRegExp;
+    }
+  });
+
+  // node_modules/lodash/_cloneSymbol.js
+  var require_cloneSymbol = __commonJS({
+    "node_modules/lodash/_cloneSymbol.js"(exports, module) {
+      var Symbol2 = require_Symbol2();
+      var symbolProto = Symbol2 ? Symbol2.prototype : void 0;
+      var symbolValueOf = symbolProto ? symbolProto.valueOf : void 0;
+      function cloneSymbol(symbol) {
+        return symbolValueOf ? Object(symbolValueOf.call(symbol)) : {};
+      }
+      module.exports = cloneSymbol;
+    }
+  });
+
+  // node_modules/lodash/_initCloneByTag.js
+  var require_initCloneByTag = __commonJS({
+    "node_modules/lodash/_initCloneByTag.js"(exports, module) {
+      var cloneArrayBuffer = require_cloneArrayBuffer();
+      var cloneDataView = require_cloneDataView();
+      var cloneRegExp = require_cloneRegExp();
+      var cloneSymbol = require_cloneSymbol();
+      var cloneTypedArray = require_cloneTypedArray();
+      var boolTag = "[object Boolean]";
+      var dateTag = "[object Date]";
+      var mapTag = "[object Map]";
+      var numberTag = "[object Number]";
+      var regexpTag = "[object RegExp]";
+      var setTag = "[object Set]";
+      var stringTag = "[object String]";
+      var symbolTag = "[object Symbol]";
+      var arrayBufferTag = "[object ArrayBuffer]";
+      var dataViewTag = "[object DataView]";
+      var float32Tag = "[object Float32Array]";
+      var float64Tag = "[object Float64Array]";
+      var int8Tag = "[object Int8Array]";
+      var int16Tag = "[object Int16Array]";
+      var int32Tag = "[object Int32Array]";
+      var uint8Tag = "[object Uint8Array]";
+      var uint8ClampedTag = "[object Uint8ClampedArray]";
+      var uint16Tag = "[object Uint16Array]";
+      var uint32Tag = "[object Uint32Array]";
+      function initCloneByTag(object, tag, isDeep) {
+        var Ctor = object.constructor;
+        switch (tag) {
+          case arrayBufferTag:
+            return cloneArrayBuffer(object);
+          case boolTag:
+          case dateTag:
+            return new Ctor(+object);
+          case dataViewTag:
+            return cloneDataView(object, isDeep);
+          case float32Tag:
+          case float64Tag:
+          case int8Tag:
+          case int16Tag:
+          case int32Tag:
+          case uint8Tag:
+          case uint8ClampedTag:
+          case uint16Tag:
+          case uint32Tag:
+            return cloneTypedArray(object, isDeep);
+          case mapTag:
+            return new Ctor();
+          case numberTag:
+          case stringTag:
+            return new Ctor(object);
+          case regexpTag:
+            return cloneRegExp(object);
+          case setTag:
+            return new Ctor();
+          case symbolTag:
+            return cloneSymbol(object);
+        }
+      }
+      module.exports = initCloneByTag;
+    }
+  });
+
+  // node_modules/lodash/_baseIsMap.js
+  var require_baseIsMap = __commonJS({
+    "node_modules/lodash/_baseIsMap.js"(exports, module) {
+      var getTag = require_getTag();
+      var isObjectLike = require_isObjectLike2();
+      var mapTag = "[object Map]";
+      function baseIsMap(value) {
+        return isObjectLike(value) && getTag(value) == mapTag;
+      }
+      module.exports = baseIsMap;
+    }
+  });
+
+  // node_modules/lodash/isMap.js
+  var require_isMap = __commonJS({
+    "node_modules/lodash/isMap.js"(exports, module) {
+      var baseIsMap = require_baseIsMap();
+      var baseUnary = require_baseUnary();
+      var nodeUtil = require_nodeUtil();
+      var nodeIsMap = nodeUtil && nodeUtil.isMap;
+      var isMap = nodeIsMap ? baseUnary(nodeIsMap) : baseIsMap;
+      module.exports = isMap;
+    }
+  });
+
+  // node_modules/lodash/_baseIsSet.js
+  var require_baseIsSet = __commonJS({
+    "node_modules/lodash/_baseIsSet.js"(exports, module) {
+      var getTag = require_getTag();
+      var isObjectLike = require_isObjectLike2();
+      var setTag = "[object Set]";
+      function baseIsSet(value) {
+        return isObjectLike(value) && getTag(value) == setTag;
+      }
+      module.exports = baseIsSet;
+    }
+  });
+
+  // node_modules/lodash/isSet.js
+  var require_isSet = __commonJS({
+    "node_modules/lodash/isSet.js"(exports, module) {
+      var baseIsSet = require_baseIsSet();
+      var baseUnary = require_baseUnary();
+      var nodeUtil = require_nodeUtil();
+      var nodeIsSet = nodeUtil && nodeUtil.isSet;
+      var isSet = nodeIsSet ? baseUnary(nodeIsSet) : baseIsSet;
+      module.exports = isSet;
+    }
+  });
+
+  // node_modules/lodash/_baseClone.js
+  var require_baseClone = __commonJS({
+    "node_modules/lodash/_baseClone.js"(exports, module) {
+      var Stack = require_Stack();
+      var arrayEach = require_arrayEach();
+      var assignValue = require_assignValue();
+      var baseAssign = require_baseAssign();
+      var baseAssignIn = require_baseAssignIn();
+      var cloneBuffer = require_cloneBuffer();
+      var copyArray = require_copyArray();
+      var copySymbols = require_copySymbols();
+      var copySymbolsIn = require_copySymbolsIn();
+      var getAllKeys = require_getAllKeys();
+      var getAllKeysIn = require_getAllKeysIn();
+      var getTag = require_getTag();
+      var initCloneArray = require_initCloneArray();
+      var initCloneByTag = require_initCloneByTag();
+      var initCloneObject = require_initCloneObject();
+      var isArray = require_isArray();
+      var isBuffer = require_isBuffer();
+      var isMap = require_isMap();
+      var isObject = require_isObject();
+      var isSet = require_isSet();
+      var keys = require_keys();
+      var keysIn = require_keysIn();
+      var CLONE_DEEP_FLAG = 1;
+      var CLONE_FLAT_FLAG = 2;
+      var CLONE_SYMBOLS_FLAG = 4;
+      var argsTag = "[object Arguments]";
+      var arrayTag = "[object Array]";
+      var boolTag = "[object Boolean]";
+      var dateTag = "[object Date]";
+      var errorTag = "[object Error]";
+      var funcTag = "[object Function]";
+      var genTag = "[object GeneratorFunction]";
+      var mapTag = "[object Map]";
+      var numberTag = "[object Number]";
+      var objectTag = "[object Object]";
+      var regexpTag = "[object RegExp]";
+      var setTag = "[object Set]";
+      var stringTag = "[object String]";
+      var symbolTag = "[object Symbol]";
+      var weakMapTag = "[object WeakMap]";
+      var arrayBufferTag = "[object ArrayBuffer]";
+      var dataViewTag = "[object DataView]";
+      var float32Tag = "[object Float32Array]";
+      var float64Tag = "[object Float64Array]";
+      var int8Tag = "[object Int8Array]";
+      var int16Tag = "[object Int16Array]";
+      var int32Tag = "[object Int32Array]";
+      var uint8Tag = "[object Uint8Array]";
+      var uint8ClampedTag = "[object Uint8ClampedArray]";
+      var uint16Tag = "[object Uint16Array]";
+      var uint32Tag = "[object Uint32Array]";
+      var cloneableTags = {};
+      cloneableTags[argsTag] = cloneableTags[arrayTag] = cloneableTags[arrayBufferTag] = cloneableTags[dataViewTag] = cloneableTags[boolTag] = cloneableTags[dateTag] = cloneableTags[float32Tag] = cloneableTags[float64Tag] = cloneableTags[int8Tag] = cloneableTags[int16Tag] = cloneableTags[int32Tag] = cloneableTags[mapTag] = cloneableTags[numberTag] = cloneableTags[objectTag] = cloneableTags[regexpTag] = cloneableTags[setTag] = cloneableTags[stringTag] = cloneableTags[symbolTag] = cloneableTags[uint8Tag] = cloneableTags[uint8ClampedTag] = cloneableTags[uint16Tag] = cloneableTags[uint32Tag] = true;
+      cloneableTags[errorTag] = cloneableTags[funcTag] = cloneableTags[weakMapTag] = false;
+      function baseClone(value, bitmask, customizer, key, object, stack) {
+        var result, isDeep = bitmask & CLONE_DEEP_FLAG, isFlat = bitmask & CLONE_FLAT_FLAG, isFull = bitmask & CLONE_SYMBOLS_FLAG;
+        if (customizer) {
+          result = object ? customizer(value, key, object, stack) : customizer(value);
+        }
+        if (result !== void 0) {
+          return result;
+        }
+        if (!isObject(value)) {
+          return value;
+        }
+        var isArr = isArray(value);
+        if (isArr) {
+          result = initCloneArray(value);
+          if (!isDeep) {
+            return copyArray(value, result);
+          }
+        } else {
+          var tag = getTag(value), isFunc = tag == funcTag || tag == genTag;
+          if (isBuffer(value)) {
+            return cloneBuffer(value, isDeep);
+          }
+          if (tag == objectTag || tag == argsTag || isFunc && !object) {
+            result = isFlat || isFunc ? {} : initCloneObject(value);
+            if (!isDeep) {
+              return isFlat ? copySymbolsIn(value, baseAssignIn(result, value)) : copySymbols(value, baseAssign(result, value));
+            }
+          } else {
+            if (!cloneableTags[tag]) {
+              return object ? value : {};
+            }
+            result = initCloneByTag(value, tag, isDeep);
+          }
+        }
+        stack || (stack = new Stack());
+        var stacked = stack.get(value);
+        if (stacked) {
+          return stacked;
+        }
+        stack.set(value, result);
+        if (isSet(value)) {
+          value.forEach(function(subValue) {
+            result.add(baseClone(subValue, bitmask, customizer, subValue, value, stack));
+          });
+        } else if (isMap(value)) {
+          value.forEach(function(subValue, key2) {
+            result.set(key2, baseClone(subValue, bitmask, customizer, key2, value, stack));
+          });
+        }
+        var keysFunc = isFull ? isFlat ? getAllKeysIn : getAllKeys : isFlat ? keysIn : keys;
+        var props = isArr ? void 0 : keysFunc(value);
+        arrayEach(props || value, function(subValue, key2) {
+          if (props) {
+            key2 = subValue;
+            subValue = value[key2];
+          }
+          assignValue(result, key2, baseClone(subValue, bitmask, customizer, key2, value, stack));
+        });
+        return result;
+      }
+      module.exports = baseClone;
+    }
+  });
+
+  // node_modules/lodash/cloneDeep.js
+  var require_cloneDeep = __commonJS({
+    "node_modules/lodash/cloneDeep.js"(exports, module) {
+      var baseClone = require_baseClone();
+      var CLONE_DEEP_FLAG = 1;
+      var CLONE_SYMBOLS_FLAG = 4;
+      function cloneDeep(value) {
+        return baseClone(value, CLONE_DEEP_FLAG | CLONE_SYMBOLS_FLAG);
+      }
+      module.exports = cloneDeep;
+    }
+  });
+
+  // node_modules/lodash/transform.js
+  var require_transform = __commonJS({
+    "node_modules/lodash/transform.js"(exports, module) {
+      var arrayEach = require_arrayEach();
+      var baseCreate = require_baseCreate();
+      var baseForOwn = require_baseForOwn();
+      var baseIteratee = require_baseIteratee();
+      var getPrototype = require_getPrototype2();
+      var isArray = require_isArray();
+      var isBuffer = require_isBuffer();
+      var isFunction = require_isFunction();
+      var isObject = require_isObject();
+      var isTypedArray = require_isTypedArray();
+      function transform(object, iteratee, accumulator) {
+        var isArr = isArray(object), isArrLike = isArr || isBuffer(object) || isTypedArray(object);
+        iteratee = baseIteratee(iteratee, 4);
+        if (accumulator == null) {
+          var Ctor = object && object.constructor;
+          if (isArrLike) {
+            accumulator = isArr ? new Ctor() : [];
+          } else if (isObject(object)) {
+            accumulator = isFunction(Ctor) ? baseCreate(getPrototype(object)) : {};
+          } else {
+            accumulator = {};
+          }
+        }
+        (isArrLike ? arrayEach : baseForOwn)(object, function(value, index, object2) {
+          return iteratee(accumulator, value, index, object2);
+        });
+        return accumulator;
+      }
+      module.exports = transform;
+    }
+  });
+
+  // shared/render/plugins/Commerce/modules/rendering.js
+  var require_rendering = __commonJS({
+    "shared/render/plugins/Commerce/modules/rendering.js"(exports) {
+      "use strict";
+      var _interopRequireDefault = require_interopRequireDefault().default;
+      Object.defineProperty(exports, "__esModule", {
+        value: true
+      });
+      exports.renderTree = exports.applySkuBoundConditionalVisibility = void 0;
+      var _extends2 = _interopRequireDefault(require_extends());
+      var _Transformers = require_Transformers2();
+      var _escape = _interopRequireDefault(require_escape());
+      var _cloneDeep = _interopRequireDefault(require_cloneDeep());
+      var _transform = _interopRequireDefault(require_transform());
+      var _constants = require_constants2();
+      var _constants2 = require_constants4();
+      var _RenderingUtils = require_RenderingUtils2();
+      var _commerceUtils = require_commerceUtils();
+      var _DynamoFormattingUtils = require_DynamoFormattingUtils();
+      var _Commerce = require_Commerce();
+      var allowedFieldTypes = (0, _extends2.default)({}, _constants2.SHARED_ALLOWED_FIELD_TYPES, {
+        "data-commerce-sku-id": ["ItemRef"]
+      });
+      var isBindingPropToFieldTypeAllowed = (bindingProperty, type) => {
+        const allowedTypes = allowedFieldTypes[bindingProperty];
+        return allowedTypes instanceof Array ? allowedTypes.indexOf(type) > -1 : allowedTypes && type in allowedTypes;
+      };
+      var getIn = (object, path) => {
+        const nextDotIndex = path.indexOf(".");
+        if (object == null) {
+          return null;
+        }
+        if (nextDotIndex !== -1) {
+          const pathPart = path.slice(0, nextDotIndex);
+          const restOfPath = path.slice(nextDotIndex + 1, path.length);
+          return getIn(object[pathPart], restOfPath);
+        }
+        return object[path];
+      };
+      var applyBindingsMutation = ({
+        bindingProperty,
+        type,
